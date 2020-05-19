a6f6e75338207ca79ad84a54d9bb420b35d736b0
Jean-Francois To <jeanfrancoisto@hotmail.com>
2020-05-19 17:02:12 -0400

    added dependencies

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mysql = require("mysql");
 
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -13,6 +14,13 @@ app.use("/api/scores", scores);
 
 const text = require("./routes/api/text");
 app.use("/api/text", text);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(__dirname + "/public/"));
+
+	app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+
+}
 
 const port = process.env.PORT || 5000;
 
